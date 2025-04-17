d80b306b3ff6a617ad95b914c0f7dba639b34994
Mueza Ejaz <159851369+Mueza-Ejaz@users.noreply.github.com>
2025-04-17 19:09:23 +0500

    set title and header

--- main.py
+++ main.py
@@ -1,3 +1,25 @@
 import streamlit as st # for web-interface
 from cryptography.fernet import Fernet # for making secure data like encrypt(lock) and decrypt(unlock)
 
+st.set_page_config(
+    page_title="DataVault ", 
+    page_icon="🔐",  # This sets a small favicon icon (optional)
+    layout="centered"    # Options: "centered" (default) or "wide"
+)
+
+def custom_css():
+    st.markdown(
+        """
+        <style>
+
+        </style>
+
+        """,
+
+        unsafe_allow_html=True,
+    )
+
+custom_css()
+
+st.header(" Encrypt Your Data, Stay Protected ")
+
